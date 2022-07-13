25a87faf4658d3bad7ef5cb702ff3cd7cb2cd803
Francisco Moreno Cantero <fmoreno@marcoaldany.com>
2022-07-13 23:12:42 +0200

    feat: set credentials and url
    store configuration and print

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import * as Docopt from 'docopt';
+import Conf from 'conf';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const { version } = require('../package.json');
@@ -9,7 +10,9 @@ const { docopt } = Docopt;
 const doc = `""Jira Cli.
 
 Usage:
-  jira-cli ship new <name>
+  jira-cli config set credentials <user> <password>
+  jira-cli config set url <address>
+  jira-cli config print
   jira-cli -h | --help
   jira-cli -v | --version
 
@@ -19,4 +22,34 @@ Options:
 
 ""`;
 
-docopt(doc, { version });
+const config = new Conf({
+  configName: 'jira-cli'
+});
+
+const main = (options) => {
+  if(options.config){
+    if(options.set){
+      if(options.credentials){
+        config.set({
+          credentials: {
+            user: options['<user>'],
+            password: options['<password>'],
+          }
+        });
+        return 1;
+      }
+      if(options.url){
+        config.set({
+          url: options['<address>']
+        });
+        return 1;
+      }
+    }
+    if(options.print){
+      console.log(config.store);
+      return 1;
+    }
+  }
+};
+
+main(docopt(doc, { version }));
