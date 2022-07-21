cae72a2bb7ec7391992bd7b2712fa6912635f5ca
Francisco <fmoreno@aligntech.com>
2022-07-21 11:39:21 +0200

    refactor: move configuration options to fp

--- src/index.js
+++ src/index.js
@@ -2,13 +2,48 @@
 
 import axios from 'axios';
 import * as Docopt from 'docopt';
+import S from 'sanctuary';
+const { get, reduce, Just, and, lift2, is, show, ifElse, I, compose, maybe } = S;
+import $ from 'sanctuary-def';
 import Conf from 'conf';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const { version } = require('../package.json');
 const { docopt } = Docopt;
+const getConfig = get(is($.Boolean))('config');
+const getSet = get(is($.Boolean))('set');
+const getCredentials = get(is($.Boolean))('credentials');
+const getUrl = get(is($.Boolean))('url');
+const getPrint = get(is($.Boolean))('print');
+const toBoolean = compose(maybe(false)(I));
 
 const toBase64 = (text) => globalThis.btoa(unescape(encodeURIComponent(text)));
+const setCredentials = options =>
+  reduce(acc => getter => lift2(and)(acc)(getter(options)))
+    (Just(true))
+    ([getConfig, getSet, getCredentials]);
+const setUrl = options =>
+  reduce(acc => getter => lift2(and)(acc)(getter(options)))
+    (Just(true))
+    ([getConfig, getUrl]);
+const printConfig = options =>
+  reduce(acc => getter => lift2(and)(acc)(getter(options)))
+    (Just(true))
+    ([getConfig, getPrint]);
+const updateCredentials = (config) => (options) => {
+  config.set({
+    credentials: {
+      user: options['<user>'],
+      password: options['<password>'],
+    }
+  });
+};
+const updateUrl = (config) => (options) => {
+  config.set({
+    url: options['<address>']
+  });
+};
+const printConfiguration = (config) => console.log(config.store);
 
 const doc = `""Jira Cli.
 
@@ -32,29 +67,9 @@ const config = new Conf({
 });
 
 const main = (options) => {
-  if (options.config) {
-    if (options.set) {
-      if (options.credentials) {
-        config.set({
-          credentials: {
-            user: options['<user>'],
-            password: options['<password>'],
-          }
-        });
-        return 1;
-      }
-      if (options.url) {
-        config.set({
-          url: options['<address>']
-        });
-        return 1;
-      }
-    }
-    if (options.print) {
-      console.log(config.store);
-      return 1;
-    }
-  }
+  ifElse(toBoolean(printConfig))(() => printConfiguration(config))(I)(options);
+  ifElse(toBoolean(setCredentials))(updateCredentials(config))(I)(options);
+  ifElse(toBoolean(setUrl))(updateUrl(config))(I)(options);
 
   if (options.issue) {
     if (options.set) {
@@ -157,7 +172,7 @@ const main = (options) => {
           },
           data: bodyValidate
         });
-        
+
         return Promise.all([develop, validate])
           .then(({ data }) => console.log(data))
           .catch(console.error)
